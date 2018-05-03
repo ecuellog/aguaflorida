781bb25205cb503bed51eec03bb894085cea4807
Edgardo Cuello <ecuellog@gmail.com>
2018-05-03 01:13:22 -0600

    more styling, eventos in angular component

--- src/app/eventos/eventos.component.ts
+++ src/app/eventos/eventos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-eventos',
@@ -9,7 +10,21 @@ export class EventosComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
-
+  /* Event description 116 characters max */
+  events = [
+  	{
+  		'title': 'Partida de Domino',
+  		'description': 'Juega domino con otro par de viejitos. Juega domino con otro par de viejitos. Juega domino con otro par de viejitos.',
+  		'date': 'Mayo 18, 2018',
+  		'place': '500 Tuscany Terrace NW',
+  		'imgUrl': '/assets/images/domino.jpg'
+  	}
+  	{
+  		'title': 'Noche de Bingo',
+  		'description': 'Diviertete con un buen juego de bingo',
+  		'date': 'Mayo 20, 2018',
+  		'place': '500 Rocky Rd NW',
+  		'imgUrl': '/assets/images/bingo.jpg'
+  	}
+  ];
 }
